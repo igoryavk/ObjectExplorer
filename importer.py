c7de47b3cf2ede13e74d1c3e8331b9ec0f601481
igoryavk <last85@list.ru>
2024-03-13 21:24:44 +0300

    Рабочий код парсера кодесис 	new file:   __pycache__/explorer.cpython-311.pyc 	new file:   __pycache__/objectExplorer.cpython-311.pyc 	new file:   importer.py 	new file:   saver.py

---
+++ importer.py
@@ -0,0 +1,21 @@
+from objectExplorer import ObjectExplorer
+from explorer import Explorer
+
+if __name__=="__main__":
+    explorer=ObjectExplorer()
+    showlist=[]
+    #explorer.createDOM("C://parse//Device.export")
+    #explorer.exploreList()
+    #explorer.printNames()
+    #explorer.ExplorePlcPrg()
+    #explorer.showTypeGuids()
+    #explorer.findAllPous()
+    #explorer.cutPous()
+    explorer.restoreDOM("C://parse//clear.xml")
+    #explorer.createPouDom("C://parse//POUS//ARCH_AND_RETAIN.xml")
+    #explorer.catchPouText()
+    #explorer.saveTexts()
+    #explorer.explorePou()
+    #explorer.exploreObjectSection()
+    #hide_list=[]
+    #explorer.findText(hide_list)
